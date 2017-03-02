65e5083fe2a7ab6718615adbb690293dba2f3070
Krita <Krita@APNAPC>
2017-03-02 19:42:02 +0000

    HELLO PROJECT

---
+++ HELLO WORLD PROJECT/HELLO WORLD PROJECT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HELLO_WORLD_PROJECT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
